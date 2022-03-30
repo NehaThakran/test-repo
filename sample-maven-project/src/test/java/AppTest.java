9b63b470f7f99b4655d05d2c515997012b92a8c7
NehaThakran <79734609+NehaThakran@users.noreply.github.com>
2022-03-30 12:02:28 +0530

    Add files via upload

---
+++ sample-maven-project/src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+//import static org.junit.Assert.assertTrue;
+//import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assert(true);
+    }
+}
